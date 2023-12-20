edd4a74f70205eebdcf49164bab8a81fa6fb1c2f
Silpa <anvitashilpa@gmail.com>
2023-12-20 16:57:49 +0530

    first commit

--- src/Form/User.js
+++ src/Form/User.js
@@ -2,7 +2,7 @@ import React, {useState,useEffect} from 'react';
 import axios from 'axios';
 
 function App() {
-  const url="http://localhost:8000/product/get/";
+  const url="http://localhost:8000/user/get/";
   const [data,setData]=useState([]);
   const fetchData=async()=>{
     let res = await axios.get(`${url}`);
@@ -19,8 +19,10 @@ function App() {
           return(
             <>
             <p>{dataObj.name}</p>
-            <p>{dataObj.rate}</p>
-            <p>{dataObj.description}</p>
+            <p>{dataObj.address}</p>
+            <p>{dataObj.email}</p>
+            <p>{dataObj.username}</p>
+            <p>{dataObj.password}</p>
            
             </>
           )
@@ -31,8 +33,4 @@ function App() {
 }
 
 export default App
-
-
-
-
 
